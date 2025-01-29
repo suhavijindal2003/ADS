c55ab679509472e0bda3a7185773dd75b61b53a4
suhavijindal2003 <suhavi.jindal03@gmail.com>
2025-01-29 12:55:52 +0530

    add

---
+++ Shorest_Palindrome214.java
@@ -0,0 +1,19 @@
+public  class Shorest_Palindrome214{
+    public static void main(String[] args) {
+        System.out.println(shortestPalindrome("aaaceabba"));
+    }
+    public static String shortestPalindrome(String s) {
+        int i=0;
+        int n = s.length();
+        for (int j = 0; j < n; j++) {
+            if (s.charAt(i) == s.charAt(n - j - 1)) {
+                i++;
+            }
+        } 
+        if (i == n) {
+            return s;
+        }
+        String p = new StringBuilder(s.substring(i, n)).reverse().toString();
+        return p + shortestPalindrome(s.substring(0, i)) + s.substring(i);
+    }
+}
