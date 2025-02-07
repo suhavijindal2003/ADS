b6b7c8556de2e9c8fee69af0f0adca594e01237e
suhavijindal2003 <suhavi.jindal03@gmail.com>
2025-02-07 10:14:22 +0530

    add

---
+++ String_permutation.java
@@ -0,0 +1,23 @@
+
+public class String_permutation {
+  
+    public static void main(String[] args) {
+        String s="abcd";
+        System.out.println("permutation of "+s+"is");
+        generate(s, "");
+    }
+
+
+
+    public static void generate(String s,String perm){
+        if(s.isEmpty()){
+            System.out.println(perm);
+            return;
+        }
+        for(int i=0;i<s.length();i++){
+            Character ch=s.charAt(i);
+            String rem=s.substring(0, i)+s.substring(i+1);
+            generate(rem, perm+ch);
+        }
+    }
+}
